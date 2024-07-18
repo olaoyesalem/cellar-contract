f9c53ec9227539bbbd97cfa318d6d8aa0fc1200e
olaoyesalemgreat <olaoyesalemgreat@gmail.com>
2024-07-18 15:13:21 +0000

    Created Contract

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
-
+//SafeTransferLib.sol
+import { SafeTransferLib } from "../lib/solmate/src/utils/SafeTransferLib.sol";
 contract Counter {
     uint256 public number;
 
